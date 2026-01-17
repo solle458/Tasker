a4335da9e83e32a50f763163b70d4cdc7cc3553d
solle458 <139096138+solle458@users.noreply.github.com>
2026-01-17 11:45:09 +0900

    feat(): Implement sample to use gemini

---
+++ sample/sample_gemini.py
@@ -0,0 +1,14 @@
+from google import genai
+from dotenv import load_dotenv
+from pathlib import Path
+import os
+
+dotenv_path = Path(__file__).parent.parent / '.env'
+load_dotenv(dotenv_path)
+# The client gets the API key from the environment variable `GEMINI_API_KEY`.
+client = genai.Client(api_key=os.environ.get("GEMINI_API_KEY"))
+
+response = client.models.generate_content(
+    model="gemini-3-flash-preview", contents="Explain how AI works in a few words"
+)
+print(response.text)
